e4bef2f631ae6634f44118f94f5729e68d0000c3
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-08 17:03:11 +0530

    Delete alertComp.js

--- components/alertComp.js
+++
@@ -1,8 +0,0 @@
-Vue.component('alert-comp',{
-template : `<div>
-				<div class="alert alert-danger" role="alert" v-if='visibility === "true"'>
-					<strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
-				</div>
-			</div>`,
-props: ['visibility'],
-})
