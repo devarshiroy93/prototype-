02730cb09bcfe864f61394c4d0912e3e5ed7718e
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-09 17:47:20 +0530

    added firebase config file
    see above

---
+++ vPrj/config/fbaseConnect.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDYCShuj_22kHA18Bi6mwD7967A8zBGlCs",
+    authDomain: "prototype-bk.firebaseapp.com",
+    databaseURL: "https://prototype-bk.firebaseio.com",
+    projectId: "prototype-bk",
+    storageBucket: "prototype-bk.appspot.com",
+    messagingSenderId: "946117321289"
+  };
+  firebase.initializeApp(config);
