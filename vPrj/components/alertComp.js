447329694f79ce2f2a1dc1cbb6f8c423ecd3b0b2
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-10 13:36:54 +0530

    Update alertComp.js

--- vPrj/components/alertComp.js
+++ vPrj/components/alertComp.js
@@ -1,6 +1,6 @@
 Vue.component('alert-comp',{
 template : `<div>
-				<div class="alert alert-danger" role="alert" v-if='visibility'>
+				<div class="alert alert-danger" role="alert" v-if='visibility' id="alert">
 					<strong>Oh snap!</strong> Something went wrong. Try submitting again.
 				</div>
 			</div>`,
