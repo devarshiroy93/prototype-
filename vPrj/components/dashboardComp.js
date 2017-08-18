8aafdd51bfdbbed5635081ddf227820ca0c8f2c4
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-18 14:56:13 +0530

    updated dashboard js

--- vPrj/components/dashboardComp.js
+++ vPrj/components/dashboardComp.js
@@ -1,8 +1,10 @@
 var dashboardComp = Vue.component('dash-comp',{
 props : ['user'],
 template : `<div>
-                <div class ="col-md-3 col-sm-3 col-xs-12 sidebar-content"><sidebar-comp :userData = user.providerData[0]></sidebar-comp></div>
-                <div class="col-md-7 col-sm-7 col-xs-12">
+                <div class ="col-md-3 col-sm-3 col-xs-12 sidebar-content">
+                    <sidebar-comp :userData = user.providerData[0]></sidebar-comp>
+                </div>
+                <div class="col-md-7 col-sm-7 col-xs-12 user-posts">
                     <div v-if="user.providerData[0] !== undefined" >
 						<create-post :userUid=user.uid></create-post>
 						<post-card :userUid=user.uid></post-card>
@@ -15,7 +17,7 @@ template : `<div>
         if(this.$route.params.user === undefined){
             router.push('login')
         }
-		checkIfUserExists(this.$route.params.user.uid, this.$route.params.user);//this function checks if user exits in database or not .If not then pushes user data into database.
+		//checkIfUserExists(this.$route.params.user.uid, this.$route.params.user);//this function checks if user exits in database or not .If not then pushes user data into database.
     }
 										
 })
