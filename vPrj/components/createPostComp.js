4eb7d659ee47bdfb6b668edd463b1255dab21441
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-18 15:00:11 +0530

    updated

--- vPrj/components/createPostComp.js
+++ vPrj/components/createPostComp.js
@@ -1,14 +1,14 @@
 Vue.component('create-post',{
 	props : ['userUid'],
 	template  :`<div>
-					<div>
-					<button type="button" class="btn btn-sm btn-success subheader create-btn"><i class="material-icons">add_circle_outline</i>CREATE</button>
-					<div class="form-group post-form">
-						<label for="title">Title</label>
-						<input type="text" class="form-control input-sm" id="title" v-model ="titleContent">
-						<label for="PostBody"></label>
-						<textarea class="form-control" rows="3" id="PostBody" v-model ="bodyContent"></textarea>
-					</div>
+	<button type="button" class="btn btn-sm btn-success subheader create-btn"><i class="material-icons">add_circle_outline</i>CREATE</button>
+					<div class="post-form">
+						<div class="form-group">
+							<label for="title">Title</label>
+							<input type="text" class="form-control input-sm" id="title" v-model ="titleContent">
+							<label for="PostBody"></label>
+							<textarea class="form-control" rows="3" id="PostBody" v-model ="bodyContent"></textarea>
+						</div>
 					<button type="button" class="btn btn-default" v-on:click = createPost()>Post</button>
 					</div>
 				</div>`,
