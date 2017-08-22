2a1462feb3bda3982833a51abf62394bebfee893
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-22 12:39:21 +0530

    made alert messages with configurable messages
    see above

--- vPrj/components/alertComp.js
+++ vPrj/components/alertComp.js
@@ -1,18 +1,30 @@
 Vue.component('alert-comp',{
 template : `<div>
-				<div class="alert" :class="classes[state]" role="alert" v-if='visibility' id="alert">
-					<strong>Oh snap!</strong> Something went wrong. Try submitting again.
+				<div class="alert" :class="classes[state]" role="alert" v-if='showAlert' id="alert">
+					<a v-on:click ="closeAlert()" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<strong v-if='state==="error"'>Oh snap!</strong>  <span  v-if='state==="info"'><span></span>Hi<span class="alertFont">{{userName}}!</span></span> <span>{{messages[state]}}</span>
+					
 				</div>
 			</div>`,
     
-props: ['visibility','state'],
+props: ['visibility','state','userName'],
     
 data: function () {
 			return {
 				classes :{
                     'error': 'alert-danger',
-                    'info' : 'alert-info alert-dismissable fade in'
-                }
+                    'info' : 'alert-info alert-dismissable '
+                },
+				messages :{
+					'error' : 'Something went wrong .Try submitting again',
+					'info'  : 'Welcome to Proto'
+				},
+				showAlert  : this.visibility
 			}
 		},
+methods : {
+	closeAlert : function(){
+		this.showAlert = false
+	}
+}
 })
