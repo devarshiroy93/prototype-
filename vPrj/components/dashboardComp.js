f27088dc0674df003510995299ca9143f3564edc
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-23 10:14:47 +0530

    updated

--- vPrj/components/dashboardComp.js
+++ vPrj/components/dashboardComp.js
@@ -1,7 +1,7 @@
 var dashboardComp = Vue.component('dash-comp',{
 props : ['user'],
 template : `<div class="container dashboard">
-                <div class ="col-md-2 col-sm-3 col-xs-12 col-lg-3 sidebar-content"><sidebar-comp :userData = user.providerData[0]></sidebar-comp></div>
+                <div class ="col-md-3 col-sm-3 col-xs-12 col-lg-3 hidden-xs sidebar-content"><sidebar-comp :userData = user.providerData[0]></sidebar-comp></div>
                 <div class="col-md-7 col-sm-8 col-xs-12 user-posts">
                     <div v-if="user.providerData[0] !== undefined" >
                         <div v-if ="user.emailVerified"><alert-comp :visibility = user.emailVerified :state = "state" :userName = user.providerData[0].displayName></alert-comp></div>
