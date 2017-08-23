b90e87d90be37b82aa56a6406597aa90d3ceeeec
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-23 10:15:41 +0530

    updated

--- vPrj/app.js
+++ vPrj/app.js
@@ -21,12 +21,15 @@ var firstComponent  = Vue.component('login-reg',{
                         <div class="row">
                             <div class="col-lg-12">
                                 <form id="login-form" role="form" class="" v-if='view=="login"'>
-                                    <div class="form-group">
-                                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" required>
-                                    </div>
+                                   <div class="input-group my-input form-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email" required>
+                                </div>
+
+                                <div class="input-group my-input form-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input id="login-password" type="password" class="form-control" name="password" placeholder="password" required>
+                                </div>
                                     <div class="form-group text-center">
                                         <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
                                         <label for="remember"> Remember Me</label>
