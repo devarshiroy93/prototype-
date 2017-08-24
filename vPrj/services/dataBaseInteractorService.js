ea369e09fc970d18caaf943a8d170011b456e912
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-24 19:06:25 +0530

    updated with comments interaction with firebase

--- vPrj/services/dataBaseInteractorService.js
+++ vPrj/services/dataBaseInteractorService.js
@@ -40,3 +40,17 @@ function pushPostIntoDatabase(postBody,user,photoURL) {
 	})
 	
 };
+
+function pushCommentsIntoDataBase(commentData,id){
+	var id = id;
+	console.log(commentData);
+	return firebase.database().ref('comments/'+ id).push({
+		'author' : commentData.author,
+		'body' : commentData.body,
+		'timeStamp' : commentData.timeStamp
+	}).then(function(result){
+		return true
+	}).catch(function(error){
+		return false
+	})
+}
