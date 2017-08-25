f87b7dcf12231c3c521447e917ca2197b259b015
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-25 14:53:09 +0530

    Added comment List comp

---
+++ vPrj/components/commentListComp.js
@@ -0,0 +1,14 @@
+Vue.component('comment-list',{
+	props : [],
+	template : `<div>
+					<div>
+						<div class = "row">
+						<div class = "col-md-2"></div>
+						<div class = "col-md-8 col-sm-8 col-xs-8">
+						<p>dummy Comment By devarshi</p>
+						</div>
+						<div class = "col-md-2"></div>
+						</div>
+					</div>
+				</div>`
+})
