8ed9337d7f387c1ab221d14f496d658de607eba6
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-25 18:52:43 +0530

    updated for determining mobile view

--- vPrj/store.js
+++ vPrj/store.js
@@ -1,15 +1,22 @@
 const store = new Vuex.Store({
 	state: {
 		user :{},
+		isMobile : false
 	},
 	mutations  : {
 		assignCurrentUser (state,user){
-			state.user = user
+			state.user = user;
+		},
+		assignView (state,view){
+			state.isMobile = view;
 		}
 	},
 	getters :{
 		getCurrentUser (state){
 			return state.user;
+		},
+		getCurrentView(state){
+			return state.isMobile;
 		}
 	}
   
