e0c6970b101816c57f048ce40125eb21b55a14dd
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-25 11:34:09 +0530

    updated

--- vPrj/components/IndividualPostComp.js
+++ vPrj/components/IndividualPostComp.js
@@ -10,7 +10,7 @@ var individualPostcomp = Vue.component('singular-page',{
 				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12" >
                      <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
                         <img :src=data.authorPic alt="Card image cap" class="img-responsive">
-                     </div>
+                        </div>
                          <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
                              <h4 class="dashboard-card-title col-md-12 col-sm-12 col-xs-12 subheader">{{data.title}}<span class="post-time caption">{{data.timeStamp}}</span></h4>
                             <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{data.body}}</p>
@@ -18,8 +18,8 @@ var individualPostcomp = Vue.component('singular-page',{
                             <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">thumb_up</i>Likes</a>
                             <a class="col-md-4 col-xs-4 col-sm-4 col-lg-4 comments"><i class="material-icons">chat_bubble_outline</i>Comments</a>
                             <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">share</i>Share</a></div>
-                           </div>
-                 </div>
+                         </div>
+                     </div>
 				 
                         <!-- Card Ends-->
 				</div>
