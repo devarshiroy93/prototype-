5ddea62343b03301cd7d9801646706b164cf989b
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-25 18:53:48 +0530

    updated dashboard

--- vPrj/components/dashboardComp.js
+++ vPrj/components/dashboardComp.js
@@ -1,6 +1,7 @@
 var dashboardComp = Vue.component('dash-comp',{
+
 template : `<div class="container dashboard">
-                <div class ="col-md-3 col-sm-3 col-xs-12 col-lg-3 hidden-xs sidebar-content"><sidebar-comp :userData = data.providerData[0]></sidebar-comp></div>
+                <div class ="col-md-3 col-sm-3 col-xs-12 col-lg-3  sidebar-content" :class = 'mobile'><sidebar-comp :userData = data.providerData[0]></sidebar-comp></div>
                 <div class="col-md-7 col-sm-8 col-xs-12 user-posts">
                     <div v-if="data.providerData[0] !== undefined" >
                         <div v-if ="data.emailVerified"><alert-comp :visibility = data.emailVerified :state = "state" :userName = data.providerData[0].displayName></alert-comp></div>
@@ -12,7 +13,9 @@ template : `<div class="container dashboard">
     data: function () {
 			return {
                 state : 'info',
-                data : store.getters.getCurrentUser
+                data : store.getters.getCurrentUser,
+				isMobileView : store.getters.getCurrentView,
+				mobile :''
 			}
 		},
 
@@ -21,6 +24,7 @@ template : `<div class="container dashboard">
             router.push('login')
         }
 		checkIfUserExists(this.data.uid, this.data.user);//this function checks if user exits in database or not .If not then pushes user data into database.
+		this.isMobileView ? this.mobile = 'hidden-xs' :  this.mobile = '';
     }
 										
 })
