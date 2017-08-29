e04601ab919ce91f61962a4d304417ecdff22f88
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-29 15:27:32 +0530

    updated

--- vPrj/components/IndividualPostComp.js
+++ vPrj/components/IndividualPostComp.js
@@ -14,10 +14,10 @@ var individualPostcomp = Vue.component('singular-page', {
                          <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
                              <h4 class="dashboard-card-title col-md-12 col-sm-12 col-xs-12 subheader">{{postData.authorName}}<span class="post-time caption">{{postData.timeStamp}}</span></h4>
                             <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{postData.body}}</p>
-                           <div class="row caption">
-                            <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">thumb_up</i>Likes</a>
-                            <a class="col-md-4 col-xs-4 col-sm-4 col-lg-4 comments"><i class="material-icons">chat_bubble_outline</i>Comments</a>
-                            <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">share</i>Share</a></div>
+                           <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
+                            <a class="col-md-3 col-xs-4 col-sm-4 col-lg-3 comments"><i class="material-icons">chat_bubble_outline</i>Comments</a>
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3  "><i class="material-icons">share</i>Share</a></div>
                          </div>
                      </div>
 				 
