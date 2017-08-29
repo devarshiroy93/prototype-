fb8d5fd61439c78bd601ad2e9171f6ca0d3ffd51
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-29 15:27:11 +0530

    updated

--- vPrj/components/postCardComp.js
+++ vPrj/components/postCardComp.js
@@ -4,17 +4,17 @@ Vue.component('post-card',{
 				<!-- Card Starts-->
 				<div v-if='postData' >
 				
-				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12" v-for='post in postData'  v-on:click = goToIndividualPage(post)>
+				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12 col-lg-12" v-for='post in postData'  v-on:click = goToIndividualPage(post)>
                      <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
                         <img :src="post.authorPic" alt="Card image cap" class="img-responsive">
                      </div>
                          <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
 						 <div class="col-md-12 col-sm-12 col-xs-12 cardUserName"><span class="subheader userName dashboard-card-title">{{post.authorName}}</span><span class="post-time caption">{{post.timeStamp}}</span></div>
                             <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{post.body}}</p>
-                           <div class="row caption">
-                            <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">thumb_up</i>Likes</a>
-                            <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4 comments" v-on:click=goToIndividualPage(post)><i class="material-icons">chat_bubble_outline</i>Comments</a>
-                            <a  class="col-md-4 col-xs-4 col-sm-4 col-lg-4"><i class="material-icons">share</i>Share</a></div>
+                           <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3 comments" v-on:click=goToIndividualPage(post)><i class="material-icons">chat_bubble_outline</i>Comments</a>
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">share</i>Share</a></div>
                            </div>
 
                         </div>
