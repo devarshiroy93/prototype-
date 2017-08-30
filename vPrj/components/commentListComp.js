7fc6d78ea4731cedc3c7a15b6e880b59c8f184c2
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-30 20:02:20 +0530

    updated

--- vPrj/components/commentListComp.js
+++ vPrj/components/commentListComp.js
@@ -2,17 +2,20 @@ Vue.component('comment-list', {
 	props : ['postKey'],
     template: `<div>
 					<div v-for = 'com in commentList'>
+					<loader-comp v-if="!showComments" state = 'loading' size='small'></loader-comp>
+					<div  v-if="showComments">
 						<div class = "col-md-10 col-sm-10 col-xs-11 col-lg-10 otherUserComment">
 							<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage">
 								
 							</div>
 							<div class="col-md-8 col-sm-8 col-xs-10 col-lg-10">
-								<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{com.author}}<span class="post-time caption">{{com.timeStamp}}</span></h5>
+								<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{com.authorName}}<span class="post-time caption">{{com.timeStamp}}</span></h5>
 								<p class="commentText col-md-12 col-sm-12 col-xs-12 body3">{{com.body}}</p>
 								<div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
 									<a class="col-md-4 col-xs-6 col-sm-4 col-lg-4"><i class="material-icons">thumb_up</i>Likes</a>
 									<a class="col-md-4 col-xs-6 col-sm-4 col-lg-4 comments"><i class="material-icons">reply</i>Reply</a>
 								</div>
+						</div>		
 							</div>
 						</div>
 					</div>
@@ -21,7 +24,8 @@ Vue.component('comment-list', {
 		return {
 			commentList : [],
 			comments : [],
-			authors : []
+			authors : [],
+			showComments : false
 		}
 	},
     methods : {
@@ -29,12 +33,13 @@ Vue.component('comment-list', {
 		   for(var i=0;i<comments.length;i++ ){
 			   for(var x=0;x<authors.length;x++ ){
 			   if(comments[i].author === authors[x].uid){
-				   comments[i].author = authors[x].displayName;
+				   comments[i].authorName = authors[x].displayName;
 			   }
 			}
 		   }
 		   console.log(comments);
            this.commentList = comments;
+		   this.showComments = true;
 	   }
     },
     created : function(){
