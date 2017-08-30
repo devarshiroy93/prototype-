a48faceb1ad29201cf1c6a04f7fc1dc9620e3c6a
devarshiroy93 <devarshiroy93@gmail.com>
2017-08-30 20:06:04 +0530

    updated for loader of different sizes

--- vPrj/components/loaderComp.js
+++ vPrj/components/loaderComp.js
@@ -1,8 +1,12 @@
 Vue.component('loader-comp', {
-    props: ['state'],
+    props: ['state','size'],
     template: `<div>
-                    <div v-if='state === "loading"'>
+                    <div v-if='state === "loading" && size === "big"'>
                         <div class ="loader"></div>
                     </div>
+					<div v-if='state === "loaderSmall" && size === "small"'>
+                        <div class ="loader"></div>
+                    </div>
+					
                 </div>`
 })
