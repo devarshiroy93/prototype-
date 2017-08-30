412685fe03163b52a941609ca88d791f15a2d24c
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-08-30 18:51:53 +0530

    added snackbar js

---
+++ vPrj/components/snackbarComp.js
@@ -0,0 +1,11 @@
+Vue.component('snackbar-comp',{
+    props:[],
+    'template':`
+        <div>
+            <div id="snackbar">Your comment has been posted successfully.</div>
+        </div>
+    `,
+    created:function(){
+
+    }
+})
