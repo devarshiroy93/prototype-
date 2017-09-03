2a29acbd8f3362679c7759f9b714dcbd39b40904
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-09-03 12:46:13 +0530

    fixed comment distorting issue

--- vPrj/components/IndividualPostComp.js
+++ vPrj/components/IndividualPostComp.js
@@ -11,12 +11,12 @@ var individualPostcomp = Vue.component('singular-page', {
                      <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
                         <img :src=postData.authorPic alt="Card image cap" class="img-responsive">
                         </div>
-                         <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
+                         <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10 commentBody">
                              <h4 class="dashboard-card-title col-md-12 col-sm-12 col-xs-12 subheader">{{postData.authorName}}<span class="post-time caption">{{postData.timeStamp}}</span></h4>
                             <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{postData.body}}</p>
                            <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
                             <a  class="col-md-3 col-xs-4 col-sm-3 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
-                            <a class="col-md-3 col-xs-4 col-sm-3 col-lg-3 comments"><i class="material-icons">chat_bubble_outline</i><span v-if="postData.commentCount>0"><b>{{postData.commentCount}} </b></span>Comment<span v-if ="postData.commentCount>1">s</span></a>
+                            <a class="col-md-3 col-xs-4 col-sm-3 col-lg-3 comments"><i class="material-icons">chat_bubble_outline</i><span v-if="postData.commentCount>0" class="commentCount"><strong>{{postData.commentCount}} </strong></span>Comment<span v-if ="postData.commentCount>1">s</span></a>
                             <a  class="col-md-3 col-xs-4 col-sm-3 col-lg-3  "><i class="material-icons">share</i>Share</a></div>
                          </div>
                      </div>
--- vPrj/components/postCardComp.js
+++ vPrj/components/postCardComp.js
@@ -8,12 +8,12 @@ Vue.component('post-card',{
                      <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
                         <img :src="post.authorPic" alt="Card image cap" class="img-responsive">
                      </div>
-                         <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
+                         <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10 commentBody">
 						 <div class="col-md-12 col-sm-12 col-xs-12 cardUserName"><span class="subheader userName dashboard-card-title">{{post.authorName}}</span><span class="post-time caption">{{post.timeStamp}}</span></div>
                             <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{post.body}}</p>
                            <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
                             <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
-                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3 comments" v-on:click=goToIndividualPage(post)><i class="material-icons">chat_bubble_outline</i><span v-if="post.commentCount>0"><b>{{post.commentCount}} </b></span>Comment<span v-if ="post.commentCount>1">s</span></a>
+                            <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3 comments" v-on:click=goToIndividualPage(post)><i class="material-icons">chat_bubble_outline</i><span v-if="post.commentCount>0"><strong>{{post.commentCount}} </strong></span>Comment<span v-if ="post.commentCount>1">s</span></a>
                             <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">share</i>Share</a></div>
                            </div>
 
