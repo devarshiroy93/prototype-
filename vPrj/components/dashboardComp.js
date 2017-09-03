2b6472ba985325209221e6cda80a0fcc43b3e0fb
devarshiroy93 <devarshiroy93@gmail.com>
2017-09-03 12:44:49 +0530

    fixed a bug relating to sidebar component

--- vPrj/components/dashboardComp.js
+++ vPrj/components/dashboardComp.js
@@ -24,6 +24,7 @@ var dashboardComp = Vue.component('dash-comp', {
             router.push('login')
         }
        this.data.uid ? checkIfUserExists(this.data.uid, this.data):''; //this function checks if user exits in database or not .If not then pushes user data into database.
+        console.log(this.isMobileView);
         this.isMobileView ? this.mobile = 'hidden-xs' : this.mobile = '';
 
         store.watch(function (state) {
