d7b552557429a43978d95baf539f18e9221d3ee8
devarshiroy93 <devarshiroy93@gmail.com>
2017-09-05 18:01:41 +0530

    updated

--- vPrj/components/postCardComp.js
+++ vPrj/components/postCardComp.js
@@ -4,13 +4,13 @@ Vue.component('post-card',{
 				<!-- Card Starts-->
 				<div v-if='postData' >
 				
-				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12 col-lg-12" v-for='post in postData'  v-on:click = goToIndividualPage(post)>
+				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12 col-lg-12" v-for='post in postData' >
                      <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
                         <img :src="post.authorPic" alt="Card image cap" class="img-responsive">
                      </div>
                          <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10 commentBody">
 						 <div class="col-md-12 col-sm-12 col-xs-12 cardUserName"><span class="subheader userName dashboard-card-title">{{post.authorName}}</span><span class="post-time caption">{{post.timeStamp}}</span></div>
-                            <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{post.body}}</p>
+                            <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{post.body}}<span v-if="post.isChopped">...<readmore-comp v-on:redirect="goToIndividualPage($event)":postData = post></readmore-comp></span></p>
                            <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
                             <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
                             <a  class="col-md-3 col-xs-4 col-sm-4 col-lg-3 comments" v-on:click=goToIndividualPage(post)><i class="material-icons">chat_bubble_outline</i><span v-if="post.commentCount>0" class="commentCount"><strong>{{post.commentCount}} </strong></span>Comment<span v-if ="post.commentCount>1">s</span></a>
