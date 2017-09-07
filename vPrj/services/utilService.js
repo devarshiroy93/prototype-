3cbc8d333dad906d306e4d7c9b2c6fe28dd136e4
devarshiroy93 <devarshiroy93@gmail.com>
2017-09-07 13:59:56 +0530

    updated (more precise timeStamp processing)

--- vPrj/services/utilService.js
+++ vPrj/services/utilService.js
@@ -13,15 +13,7 @@ function processTimeStamp(data){
 	if(postDate.getFullYear() === today.getFullYear()){
 			if(postDate.getMonth() === today.getMonth()){
 				if(postDate.getDate() === today.getDate()){
-					if(postDate.getHours() === today.getHours()){
-						if(postDate.getMinutes() === today.getMinutes()){
-							processedDate = processSeconds(data)
-						}else{
-							processedDate = processMinutes(data);
-						}
-					}else{
-						processedDate = processHours(data);
-				}
+					processedDate = calculateMinutesAndHours(data)
 				}else{
 					processedDate = processMonth(data);
 				}
@@ -42,25 +34,35 @@ function processMonth(date){
 	return difference
 }
 
-function processHours(date){
+function calculateMinutesAndHours(date){
 	var postDate =  new Date(date);
 	var today =new Date(Date.now());
-	var difference 
-	today.getHours()-postDate.getHours() === 1?  difference = today.getHours()-postDate.getHours()+' hour ago': difference = today.getHours()-postDate.getHours()+' hours ago';
-	return difference
-}
-function processMinutes(date){
-	var postDate =  new Date(date);
-	var today =new Date(Date.now());
-	var difference 
-	today.getMinutes()-postDate.getMinutes() === 1?  difference = today.getMinutes()-postDate.getMinutes()+' minute ago': difference = today.getMinutes()-postDate.getMinutes()+' minutes ago';
-	return difference
-}
-function processSeconds(date){
-	var postDate =  new Date(date);
-	var today =new Date(Date.now());
-	var difference 
-	today.getSeconds()-postDate.getSeconds() === 1?  difference = today.getSeconds()-postDate.getSeconds()+' second ago': difference = today.getSeconds()-postDate.getSeconds()+' seconds ago';
-	difference === '0 seconds ago' ? difference = 'just now' : ''
+	var postDateHours = postDate.getHours();
+	var postDateMinutes = postDate.getMinutes();
+	var difference;
+	 if(today.getHours() - postDateHours >= 1) {
+		 difference =  (today.getHours() - postDateHours)*60 + Math.abs(postDateMinutes-today.getMinutes()) 
+		 }
+	 else{
+		 difference = Math.abs(postDateMinutes-today.getMinutes());
+		 }
+	 
+	if(difference >60) {
+		difference = (difference/60).toString().charAt(0) 
+		if(difference > 1){
+			difference += 'hours ago'
+		}
+		else{
+			difference += ' hour ago'
+		}
+		
+	}else if(difference === 0){
+		difference  = 'Just now'
+	}
+	else {
+		
+		difference += " minutes ago"
+	}
+	
 	return difference
 }
