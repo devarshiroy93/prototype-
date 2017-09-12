4b2f4fbcb9587f214c1edc97dcafe2f3189800b4
Chandan <chndnchoudhary22@gmail.com>
2017-09-12 17:05:23 +0530

    Friend icon added

--- vPrj/components/easyFriendComp.js
+++ vPrj/components/easyFriendComp.js
@@ -1,5 +1,5 @@
 //Component to send contact request from posts
 Vue.component('easy-friend',{
-	'template' : `<div><div class="sendFreindReq">icon</div></div>`,
+	'template' : `<div class="addFriend"><button title="Add as Friend"><i class="material-icons">person_add</i></button></div>`,
 	
 })
