a3c5ee802c38520f4f7e3c45240659f33e72ff3a
Devarshi Roy <devarshiroy93@gmail.com>
2017-09-13 16:05:42 +0530

    functionality for unshowing snackbar

--- vPrj/components/snackbarComp.js
+++ vPrj/components/snackbarComp.js
@@ -2,7 +2,7 @@ Vue.component('snackbar-comp',{
     props:['triggered','action'],
     'template':`
         <div>
-            <div id="snackbar" v-if = "triggered" :class="{ show: triggered }"><div>{{messages[action]}}</div><a  >&times;</a></div>
+            <div id="snackbar" v-if = "triggeredComputed" :class="{ show: triggered }"><div>{{messages[action]}}</div></div>
         </div>
     `,
 	 data: function () {
@@ -10,10 +10,21 @@ Vue.component('snackbar-comp',{
             messages : {
 				addFriend : 'Friend Request Sent!',
 				addComment : 'Comment has been added'
-			}
+			},
+			triggeredComputed : false
         }
     },
-    created:function(){
-		
-    }
+	watch :{
+		triggered : function(val){
+			this.triggeredComputed = val; 
+			if(val){
+				setTimeout(function () {
+					this.triggeredComputed = false
+				}.bind(this), 3000)
+			};
+		}
+	},
+	created : function(){
+		this.triggeredComputed = this.triggered
+	}
 })
