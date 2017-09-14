2620316adaddd71ace6b242d3cca62a9b83829cb
Devarshi Roy <devarshiroy93@gmail.com>
2017-09-14 17:44:24 +0530

    freind request changes

--- vPrj/components/friendListComp.js
+++ vPrj/components/friendListComp.js
@@ -5,9 +5,9 @@ var friendListComp = Vue.component('friend-list',{
 				<div class="col-md-8 col-sm-12 col-xs-12" >Friend Requests
 				<ul class = "list-group">
 					<div v-if="showRequestSection">
-						<div>
-						
-						</div>
+						<div v-for = "request in friendRequests" >
+							<div>{{request.displayName}}</div>
+							<p>{{request.email}}</p>
 					</div>
 				</ul>
 				</div>
@@ -15,8 +15,25 @@ var friendListComp = Vue.component('friend-list',{
 			</div>`,
  data: function () {
         return {
-			showRequestSection : false
+			showRequestSection : false,
+			friendRequests : []
         }
     },
+	methods : {
+		fetchUsersInfo : function(from){
+			firebase.database().ref('users').child(from).once('value').then(function(snapshot){
+				console.log(snapshot.val());
+				this.showRequestSection = true
+				this.friendRequests.push(snapshot.val())
+			}.bind(this))
+		}
+	},
+	created : function(){
+		this.$route.params.id
+		firebase.database().ref('friendRequests/'+this.$route.params.id).on('child_added',function(snapshot){
+			console.log(snapshot.val());
+			this.fetchUsersInfo(snapshot.val().from);
+			}.bind(this))
+	}
 }
 )
