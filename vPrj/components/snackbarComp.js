37e3237f28b0b122edbdc85ea601dea9179e4123
Chandan <chndnchoudhary22@gmail.com>
2017-09-14 13:31:28 +0530

    updated snackbar styling

--- vPrj/components/snackbarComp.js
+++ vPrj/components/snackbarComp.js
@@ -2,17 +2,16 @@ Vue.component('snackbar-comp',{
     props:['triggered','action'],
     'template':`
         <div>
-<<<<<<< HEAD
-            <div id="snackbar" v-if = "triggered" :class="{ show: triggered }"><div><span><i class="material-icons">check</i></span>{{messages[action]}}</div></div>
-=======
+            <div id="snackbar" v-if = "triggered" :class="{ show: triggered }"><div class="body2"><span><i class="material-icons checkIcon">check</i></span>{{messages[action]}}<i class="material-icons frClose">close</i></div></div>
+
             <div id="snackbar" v-if = "triggeredComputed" :class="{ show: triggered }"><div>{{messages[action]}}</div></div>
->>>>>>> d7b96d53ff980c5bde5b692d2c58f225e926e414
+
         </div>
     `,
 	 data: function () {
         return {
             messages : {
-				addFriend : 'Friend Request Sent!',
+				addFriend : 'Friend Request Sent !',
 				addComment : 'Comment has been added'
 			},
 			triggeredComputed : false
