8b69c8c897430333fe803ac1bc3cb3a435f69cff
Chandan <chndnchoudhary22@gmail.com>
2017-09-15 15:29:16 +0530

    updated friend request component

--- vPrj/components/friendListComp.js
+++ vPrj/components/friendListComp.js
@@ -2,13 +2,18 @@ var friendListComp = Vue.component('friend-list',{
 'props' : ['userUid'],
 'template' : `<div>
 				<div class="col-md-2"></div>
-				<div class="col-md-8 col-sm-12 col-xs-12" >Friend Requests
+				<div class="col-md-8 col-sm-12 col-xs-12" ><span class="friendRequestText subheader">Friend Requests</span>
 				<ul class = "list-group">
 					<div v-if="showRequestSection">
-						<div v-for = "request in friendRequests" >
-							<div>{{request.displayName}}</div>
-							<p>{{request.email}}</p>
-					</div>
+						<div v-for = "request in friendRequests" class="col-md-4 col-lg-4 col-sm-4 col-xs-12 otherUserComment" >
+						<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img src="https://lh3.googleusercontent.com/-hU5QCbUp_lU/AAAAAAAAAAI/AAAAAAAAAb8/-XvzN_1BBVU/photo.jpg" alt="Card image cap" class="img-responsive"></div>
+							<div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
+								<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{request.displayName}}</h5>
+							    <p class="commentText col-md-12 col-sm-12 col-xs-12 body3">{{request.email}}</p>
+								<button type="button" class="btn btn-sm primary-btn interactive-text">Accept</button>
+								<button type="button" class="btn btn-sm btn-default interactive-text">Reject</button>
+							</div>
+						</div>
 				</ul>
 				</div>
 				<div class="col-md-2"></div>
