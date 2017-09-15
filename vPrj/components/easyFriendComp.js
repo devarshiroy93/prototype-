17917db27bd24f568042c95d77ba7d3014033b72
devarshiroy93 <devarshiroy93@gmail.com>
2017-09-16 01:23:21 +0530

    updated

--- vPrj/components/easyFriendComp.js
+++ vPrj/components/easyFriendComp.js
@@ -1,7 +1,7 @@
 //Component to send contact request from posts
 Vue.component('easy-friend',{
 	'props' : ['userId','postAuthor'],
-	'template' : `<div class="addFriend"  v-if="userId!==postAuthor"><button disabled = "friendButtonDisable" v-on:click = "addFriend" title="Add as Friend"><i class="material-icons" >person_add</i></button></div>`,
+	'template' : `<div class="addFriend"  v-if="userId!==postAuthor && friendButtonDisable"><button  v-on:click = "addFriend" title="Add as Friend"><i class="material-icons" >person_add</i></button></div>`,
 	'methods' : {
 		addFriend : function(){
 			var details = {
@@ -13,14 +13,16 @@ Vue.component('easy-friend',{
 	},
 	data :function(){
 		return{
-			friendButtonDisable : false
+			friendButtonDisable : true
 		}
 	},
 	created: function(){
 		firebase.database().ref('friendRequests/'+this.postAuthor).on('child_added',function(snapshot){
 			console.log(snapshot.val());
-			if(snapshot.val.from === this.userId){
-				this.friendButtonDisable = true
+			if(snapshot.val().from === this.userId){
+				this.friendButtonDisable = false
+			}else{
+				this.friendButtonDisable = true;
 			}
 			firebase.database().ref('friendRequests/'+this.postAuthor).off()
 		}.bind(this))
