c5be6404ff46ac963152ba50d45ebfb50f6e4587
devarshiroy93 <devarshiroy93@gmail.com>
2017-09-16 08:03:15 +0530

    changes made to friend List component

--- vPrj/components/friendListComp.js
+++ vPrj/components/friendListComp.js
@@ -6,7 +6,7 @@ var friendListComp = Vue.component('friend-list',{
 					<ul class = "list-group">
 					<div v-if="showRequestSection">
 						<div v-for = "request in friendRequests" class="col-md-4 col-lg-4 col-sm-4 col-xs-12 otherUserComment" >
-							<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img src="https://lh3.googleusercontent.com/-hU5QCbUp_lU/AAAAAAAAAAI/AAAAAAAAAb8/-XvzN_1BBVU/photo.jpg" alt="Card image cap" class="img-responsive">
+							<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img :src="request.photoURL" alt="Card image cap" class="img-responsive">
 							</div>
 							<div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
 								<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{request.displayName}}</h5>
