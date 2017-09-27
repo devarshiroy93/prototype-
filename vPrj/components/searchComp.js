6c61a1f0b236e653dc4f8efff43964341274a0a0
Chandan <chndnchoudhary22@gmail.com>
2017-09-27 17:34:10 +0530

    search comp updated

--- vPrj/components/searchComp.js
+++ vPrj/components/searchComp.js
@@ -1,3 +1,20 @@
 Vue.component('search-comp',{
-	'template' : `<div>Search comp</div>`
+	'template' : `<div>
+					<div class="searchComp">
+						<div class="row">
+							<div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+								<div class="searchContainer"> 
+									<div class="input-group stylish-input-group">
+										<input type="text" class="form-control"  placeholder="Search Proto" >
+										<span class="input-group-addon">
+											<button type="submit">
+												<span class="material-icons">search</span>
+											</button>  
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			</div>`
 })
