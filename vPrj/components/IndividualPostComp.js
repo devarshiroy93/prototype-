60ebbe77d4c0ba327bf769bd5b7ff7014bc09e43
Devarshi Roy <devarshiroy93@gmail.com>
2017-10-05 19:04:26 +0530

    decoupling singular page comp

--- vPrj/components/IndividualPostComp.js
+++ vPrj/components/IndividualPostComp.js
@@ -1,34 +1,19 @@
 var individualPostcomp = Vue.component('singular-page', {
     props: ['postData'],
-    template: `<div>
+    template: `<div><div>
                     <div class= "col-md-2"></div>
                     <div class="col-md-8 col-sm-12 col-xs-12">                                
                     <div>
 				<!-- Card Starts-->
-				<div  >
-				
-				<div class="dashboard-card-block col-md-12 col-sm-12 col-xs-12" >
-                     <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 dashboard-card-img">
-                        <img :src=postData.authorPic alt="Card image cap" class="img-responsive">
-                        </div>
-                         <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10 commentBody">
-                             <h4 class="dashboard-card-title col-md-12 col-sm-12 col-xs-12 subheader">{{postData.authorName}}<span class="post-time caption">{{postData.timeStamp}}</span></h4>
-                            <p class="dashboard-card-text col-md-12 col-sm-12 col-xs-12 body1">{{postData.body}}</p>
-                           <div class="row caption col-md-12 col-sm-12 col-xs-12 likeSection">
-                            <a  class="col-md-3 col-xs-4 col-sm-3 col-lg-3"><i class="material-icons">thumb_up</i>Likes</a>
-                            <a class="col-md-3 col-xs-4 col-sm-3 col-lg-3 comments"><i class="material-icons">chat_bubble_outline</i><span v-if="postData.commentCount>0" class="commentCount"><strong>{{postData.commentCount}} </strong></span>Comment<span v-if ="postData.commentCount>1">s</span></a>
-                            <a  class="col-md-3 col-xs-4 col-sm-3 col-lg-3  "><i class="material-icons">share</i>Share</a></div>
-                         </div>
-                     </div>
-				 
-                        <!-- Card Ends-->
+				 <post-card :post = "postData"></post-card>
+                <!-- Card Ends-->
 				</div>
                 <comment-list :postKey = postData.key></comment-list>
 				<comment-comp v-on:do-comment ="postComment($event)" :data = postData ></comment-comp>
 				
 				    </div>
                 </div><div class="col-md-2"></div>
-                </div>`,
+                </div></div>`,
     data: function () {
         return {
             comments: {}
