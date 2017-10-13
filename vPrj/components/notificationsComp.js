dfc2326312354b9b3586826380341ee84bdcc8d4
Chandan <chndnchoudhary22@gmail.com>
2017-10-13 12:22:56 +0530

    created notification comp

--- vPrj/components/notificationsComp.js
+++ vPrj/components/notificationsComp.js
@@ -1,3 +1,10 @@
 Vue.component('notification-comp',{
-    template : `<div>Notifications to come up here...</div>`
+    template : `<div>
+                    <div class="notificationComp col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                        <div class="notificationHeader subheader"><p>Notifications</p></div>
+                        <div class="notificationBody body2">
+                            <p><span><i class="material-icons">priority_high</i></span>Devarshi Roy has liked your last photo.</p>
+                        </div>
+                    </div>
+                </div>`
 })
