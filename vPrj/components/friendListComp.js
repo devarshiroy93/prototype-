6e37efbbdee82ec4164d9ba1ff9a7952887f5093
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2017-10-14 17:11:27 +0530

    updated

--- vPrj/components/friendListComp.js
+++ vPrj/components/friendListComp.js
@@ -1,32 +1,34 @@
 var friendListComp = Vue.component('friend-list',{
-'template' : `<div>
-				<div class="col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6 col-sm-offset-3 col-sm-6 col-xs-12 friendRequestPanel" v-if ="showRequestSection"><span class="friendRequestText subheader">Friend Requests</span>
-				<hr class="divider">
-					<ul class = "list-group">
-					<div v-if="showRequestSection">
-						<div v-for = "request in friendRequests" class="col-md-12 col-lg-12 col-sm-12 col-xs-12 otherUserComment friendRequest" >
-							<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img :src="request.photoURL" alt="Card image cap" class="img-responsive">
-							</div>
-							<div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
-								<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{request.displayName}}</h5>
-							    <p class="commentText col-md-12 col-sm-12 col-xs-12 body3">{{request.email}}</p>
-								<button type="button" class="btn btn-xs primary-btn interactive-text" v-on:click=acceptRequest(request.uid,request.key,request)>Accept</button>
-								<button type="button" class="btn btn-xs btn-default interactive-text">Reject</button></div>
-							</div>
+'template' : `	<div>
+					<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 friendListComp">
+						<div class="col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6 col-sm-offset-3 col-sm-6 col-xs-12 friendRequestPanel" v-if ="showRequestSection"><span class="friendRequestText subheader">Friend Requests</span>
+						<hr class="divider">
+							<ul class = "list-group">
+							<div v-if="showRequestSection">
+								<div v-for = "request in friendRequests" class="col-md-12 col-lg-12 col-sm-12 col-xs-12 otherUserComment friendRequest" >
+									<div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img :src="request.photoURL" alt="Card image cap" class="img-responsive">
+									</div>
+									<div class="col-md-10 col-sm-10 col-xs-10 col-lg-10">
+										<h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">{{request.displayName}}</h5>
+									    <p class="commentText col-md-12 col-sm-12 col-xs-12 body3">{{request.email}}</p>
+										<button type="button" class="btn btn-xs primary-btn interactive-text" v-on:click=acceptRequest(request.uid,request.key,request)>Accept</button>
+										<button type="button" class="btn btn-xs btn-default interactive-text">Reject</button></div>
+									</div>
+								</div>
+						</ul>
 						</div>
-				</ul>
-				</div>
-				<div class="col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6 col-sm-offset-3 col-sm-6 col-xs-12 friendsPanel" v-if ="showFriendsSection"><span class="friendRequestText subheader">Friends</span>
-				<hr class="divider">
-					<ul class = "list-group">
-					<div v-if="showFriendsSection">
-						<div v-for="user in friends">
-								<div><user-cards :friend = "user"></user-Cards></div>
+						<div class="col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6 col-sm-offset-3 col-sm-6 col-xs-12 friendsPanel" v-if ="showFriendsSection"><span class="friendRequestText subheader">Friends</span>
+						<hr class="divider">
+							<ul class = "list-group">
+							<div v-if="showFriendsSection">
+								<div v-for="user in friends">
+										<div><user-cards :friend = "user"></user-Cards></div>
+									</div>
 							</div>
+						</ul>
+						</div>
 					</div>
-				</ul>
-				</div>
-			</div>`,
+				</div>`,
  data: function () {
         return {
 			showRequestSection : false,
