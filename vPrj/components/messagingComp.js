b7fd354b4adf77c71cca8f27c06e5d47ed376ed9
Chandan <chndnchoudhary22@gmail.com>
2017-11-01 18:01:08 +0530

    updated message comp

--- vPrj/components/messagingComp.js
+++ vPrj/components/messagingComp.js
@@ -1,30 +1,16 @@
 var messenger = Vue.component('messaging-comp',{
     template : `<div>
-                    <div class= "col-md-12">
-                    <modal-comp :showModal = "showModal" modalContent = "getFriends"  v-on:close-modal = "closeModal" v-on:get-user = "assignUser($event)" ></modal-comp>
-                    <div class="col-md-3 messageContainer">Message list will appear here</div>
-                
-                    <div class="col-md-1 "></div>
-
-                    <div class ="col-md-8 messageContainer ">
-                        <div class ="panel panel-primary">
-                            <div class="panel-heading"><span><i class="material-icons">message</i></span><span class="pull-right"><i class="material-icons" v-on:click = "openModal">create</i></span></div>
-                             <div class ="panel-body">
-                                <div v-if ="recipientArray.length>0" v-for ="info in recipientArray">
-                                <chips :info = "info" v-on:remove-info = "removeRecipients($event)"></chips>
+                    <div class="messagePanel">
+                    <div class="col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8 col-sm-offset-2 col-sm-8 col-xs-12 friendRequestPanel">
+                        <div><span class="messageHeader subheader">Messages</span><span class="material-icons messageSearch">search</span></div>
+                        <hr class="divider">
+                        <ul class="list-group">
+                            <div>
+                                <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 otherUserComment friendRequest">
+                                    <div class="col-md-2 col-sm-2 col-xs-2 col-lg-2 commentImage"><img src="https://lh3.googleusercontent.com/-U8ToaA2kZh4/AAAAAAAAAAI/AAAAAAAAADI/z-Qjz5DJOXY/photo.jpg" alt="Card image cap" class="img-responsive"></div> <div class="col-md-10 col-sm-10 col-xs-10 col-lg-10"><h5 class="commentTitle col-md-12 col-sm-12 col-xs-12 body2">Devarshi Roy</h5> <p class="commentText messageContent col-md-12 col-sm-12 col-xs-12 body2">Hey Chandan! How r u doing?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p></div><div class="newMessageCount">2</div>
                                 </div>
                             </div>
-                        <div class="panel-footer" v-if="recipientArray.length>0">
-                            <div class="input-group">
-                                <input id="btn-input" type="text" class="form-control input-lg" placeholder="Type your message here..." />
-                                <span class="input-group-btn">
-                                    <button class="btn btn-primary btn-lg" id="btn-chat">
-                                    <i class="material-icons">send</i></button>
-                                </span>
-                            </div>
-                        </div>
-                        </div>
-                       
+                        </ul>
                     </div>
                     </div>
                 </div>`,
