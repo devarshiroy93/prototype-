f339510293721e272d4f5f22ccfd7f88999e3b0a
Chandan <chndnchoudhary22@gmail.com>
2017-12-21 11:17:21 +0530

    chatChipcomp updated

--- vPrj/components/chatChipComp.js
+++ vPrj/components/chatChipComp.js
@@ -3,7 +3,7 @@ Vue.component('chat-chip',{
     data : function(){
        return  {'class1' : '','class2' : '','class3' :''}
     },
-    template :` <div v-bind:class="{'receiverPanel':class1,'senderPanel' : class2}"><span v-bind:class="{'receiverPanelMsg':class1,'senderPanelMsg' : class2}">{{msg.text}}<span class="body3"  v-bind:class="{'receiverMsgTime':class1,'senderMsgTime' : class2}">11:58 AM</span></span></div>`,
+    template :` <div v-bind:class="{'receiverPanel':class1,'senderPanel' : class2}"><span v-bind:class="{'receiverPanelMsg':class1,'senderPanelMsg' : class2}">{{msg.text}}<span class="body3"  v-bind:class="{'receiverMsgTime':class1,'senderMsgTime' : class2}"><span>11:58 AM</span></span></span></div>`,
     methods :{
         determinePanelClass : function(){
             if(this.msg.userSenderId !== store.getters.getCurrentUser.uid){
