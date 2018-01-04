d934e238f7dbed459101414216b4af3b86c6933d
Devarshi Roy <devarshiroy93@gmail.com>
2018-01-04 11:59:37 +0530

    messaging view handling for mobile

--- vPrj/components/messagingComp.js
+++ vPrj/components/messagingComp.js
@@ -2,9 +2,9 @@ var messenger = Vue.component('messaging-comp', {
     props: [],
     template: `<div>
                     <div class="messagingComp col-md-12 col-lg-12 col-sm-12 col-xs-12" v-heightwidth-manage="'messagingWindowHeight'">
-                        <div><message-list v-on:newmessagetoggle = "showCreateMessageComp" :messageData = "messageData" @selected-user = passToMessagePanel($event) @play-notif="playNotification"></message-list></div>
-                        <div v-if = "!isMobileView"><message-panel :showComp = "!showCreateComp" :recipient="recipientUser" :convMsgList = "conversationMsgs" :typingStatus = "typingStatus"  @send-click = sendMessage($event) @typing-indicator ="makeTypingIndicatorChanges($event)"></message-panel></div>
-                        <div v-if = "!isMobileView"><create-message :showComp= "showCreateComp" @recipient-user="passToMessagePanel($event)"></create-message></div>
+                        <div v-if="showMessageList"><message-list v-on:newmessagetoggle = "showCreateMessageComp" :messageData = "messageData" @selected-user = passToMessagePanel($event) @play-notif="playNotification"></message-list></div>
+                        <div v-if = "showConversation"><message-panel :showComp = "!showCreateComp" :recipient="recipientUser" :convMsgList = "conversationMsgs" :typingStatus = "typingStatus"  @send-click = sendMessage($event) @typing-indicator ="makeTypingIndicatorChanges($event)"></message-panel></div>
+                        <div v-if = "showConversation"><create-message :showComp= "showCreateComp" @recipient-user="passToMessagePanel($event)"></create-message></div>
                         <div><audio ref="audioElm" src="asset/msgNoti.mp3"></audio></div>
                     </div>   
                 </div>`,
@@ -12,93 +12,111 @@ var messenger = Vue.component('messaging-comp', {
         return {
             showCreateComp: false,
             recipientUser: [],
-            messageData: [] ,//the data here will be rendered as message list in message component,
-            conversationMsgs : [],//contains the messages in a particular converation,
-            typingStatus : false,
-            isMobileView: store.getters.getCurrentView,// determines whether it is mobile view or not
+            messageData: [],//the data here will be rendered as message list in message component,
+            conversationMsgs: [],//contains the messages in a particular converation,
+            typingStatus: false,
+            showConversation: "",// determines whether to show conversation in mobile view or not,
+            showMessageList : true//
         }
     },
     methods: {
         showCreateMessageComp: function () {
-            this.showCreateComp = true
+            this.showCreateComp = true;
+            this.showConversation = true;
+            store.getters.getCurrentView ? this.showMessageList = false : '' ;
         },
         passToMessagePanel: function (user) {
             this.showCreateComp ? user = this.checkIfConversationExists(user) : '';
             user !== undefined ? this.recipientUser = user : '';
             this.showCreateComp = false;
-           if(store.getters.getSelectedConversation!== 'conversations/'+user.convKey){
-                this.fetchMessageListOfConversation(user) ;
+            if (store.getters.getSelectedConversation !== 'conversations/' + user.convKey) {
+                this.fetchMessageListOfConversation(user);
             }
-            user.type === 'userSelected' ? messagingService.resetUnReadMessageCountOfaParticularConversation(store.getters.getCurrentUser.uid,user.convKey) : '';
+            if (user.type === 'userSelected') {
+                messagingService.resetUnReadMessageCountOfaParticularConversation(store.getters.getCurrentUser.uid, user.convKey);
+                this.handleViews(user.type);
+            };
             this.bindTypingStatusChanges(user)
         },
         sendMessage: function (payload) {
-            var key = payload.key? payload.key : '';
+            var key = payload.key ? payload.key : '';
             delete payload.key;
-            messagingService.pushMessage(payload,key);
+            messagingService.pushMessage(payload, key);
         },
-        playNotification : function(){
+        playNotification: function () {
             this.$refs.audioElm.play();
         },
-        fetchMessageListOfConversation : function(userObj){
-           var parent ;
-           var recipient;
-           var senderId;
-           recipientId = userObj.uid;
-           senderId = store.getters.getCurrentUser.uid;
-           if(store.getters.getSelectedConversation){
-               firebase.database().ref(store.getters.getSelectedConversation).off();
-           }
-           userObj.convKey ? parent = 'conversations'+'/'+userObj.convKey :  parent = 'conversations'+'/'+recipientId+'__'+senderId ;
-           store.commit('assignSelectedConversation',parent);
-           this.conversationMsgs = [];
-           firebase.database().ref(parent).on('child_added',function(snap){
-            this.conversationMsgs.push(snap.val());
-
-            var data ={};// part of typing indicator bug fix
-            var uidArr = [];
-            data.convKey = snap.ref.parent.key;// part of typing indicator bug fix
-            uidArr = snap.ref.parent.key.split('__');// part of typing indicator bug fix
-            for(var i =0 ;i<uidArr.length;i++){// part of typing indicator bug fix
-                uidArr[i] !== store.getters.getCurrentUser.uid ?  data.uid = uidArr[i] : '' ;// part of typing indicator bug fix
+        fetchMessageListOfConversation: function (userObj) {
+            var parent;
+            var recipient;
+            var senderId;
+            var convKey;
+            convKey = userObj.convKey || userObj.key;
+            recipientId = userObj.uid;
+            senderId = store.getters.getCurrentUser.uid;
+            if (store.getters.getSelectedConversation) {
+                firebase.database().ref(store.getters.getSelectedConversation).off();
             }
-            this.bindTypingStatusChanges(data);// part of typing indicator bug fix
+            convKey ? parent = 'conversations' + '/' + convKey : parent = 'conversations' + '/' + recipientId + '__' + senderId;
+            store.commit('assignSelectedConversation', parent);
+            this.conversationMsgs = [];
+            firebase.database().ref(parent).on('child_added', function (snap) {
+                this.conversationMsgs.push(snap.val());
 
-           }.bind(this))
+                var data = {};// part of typing indicator bug fix
+                var uidArr = [];
+                data.convKey = snap.ref.parent.key;// part of typing indicator bug fix
+                uidArr = snap.ref.parent.key.split('__');// part of typing indicator bug fix
+                for (var i = 0; i < uidArr.length; i++) {// part of typing indicator bug fix
+                    uidArr[i] !== store.getters.getCurrentUser.uid ? data.uid = uidArr[i] : '';// part of typing indicator bug fix
+                }
+                this.bindTypingStatusChanges(data);// part of typing indicator bug fix
+
+            }.bind(this))
         },
-        fetchMessageList : function(){
+        fetchMessageList: function () {
             firebase.database().ref('convByUsers/' + store.getters.getCurrentUser.uid).on('child_added', function (data) {
-                messagingService.fetchLastMessageOfConverstion(data.val().parent).then(function (data) {   
+                messagingService.fetchLastMessageOfConverstion(data.val().parent).then(function (data) {
                     this.messageData.push(data);
                     this.recipientUser.convKey = data.key
                 }.bind(this));
             }.bind(this))
-         
+
         },
-        checkIfConversationExists : function(user){// checks whether conversation already exits with selected user
-            var userDetails ;
+        checkIfConversationExists: function (user) {// checks whether conversation already exits with selected user
+            var userDetails;
             userDetails = user;
-            for(var i = 0 ;i<this.messageData.length;i++){
-                if(this.messageData[i].key.toLowerCase().indexOf(user.uid.toLowerCase())!== -1){
+            for (var i = 0; i < this.messageData.length; i++) {
+                if (this.messageData[i].key.toLowerCase().indexOf(user.uid.toLowerCase()) !== -1) {
                     userDetails = this.messageData[i];
                     userDetails.displayName = user.displayName;
-                    userDetails.email = user.email ;
+                    userDetails.email = user.email;
                     userDetails.phoneNumber = user.phoneNumber
                     userDetails.photoURL = user.photoURL
-                    userDetails.uid= user.uid
-                }else{
+                    userDetails.uid = user.uid
+                } else {
                     userDetails = user;
-                }  
+                }
             }
             return userDetails;
         },
-        makeTypingIndicatorChanges :  function(data){//interacts with services to make changes in database for typing indicators
-            console.log('typingIndicator',data);
-           this.recipientUser.convKey ? messagingService.changeTypingStatus(this.recipientUser.convKey,store.getters.getCurrentUser.uid,data): '';
+        handleViews: function (type) {
+            if (type === "userSelected" && store.getters.getCurrentView) {
+                this.showConversation = true;
+                this.showMessageList = false
+            }
+            else {
+                this.showConversation = !store.getters.getCurrentView;
+            }
+
         },
-        bindTypingStatusChanges : function(data){
-            firebase.database().ref('typingStatus/'+data.convKey).child(data.uid).on('value',function(snap){
-                console.log('typingStatus',snap.val());
+        makeTypingIndicatorChanges: function (data) {//interacts with services to make changes in database for typing indicators
+            console.log('typingIndicator', data);
+            this.recipientUser.convKey ? messagingService.changeTypingStatus(this.recipientUser.convKey, store.getters.getCurrentUser.uid, data) : '';
+        },
+        bindTypingStatusChanges: function (data) {
+            firebase.database().ref('typingStatus/' + data.convKey).child(data.uid).on('value', function (snap) {
+                console.log('typingStatus', snap.val());
                 this.typingStatus = snap.val();
             }.bind(this))
         }
@@ -111,5 +129,6 @@ var messenger = Vue.component('messaging-comp', {
         var unreadMessageCount = [];
         var key;
         this.fetchMessageList();
+        this.handleViews();
     }
 })
