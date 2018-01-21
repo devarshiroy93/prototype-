7e6eadfe0236884d220106c57d7268df80971c23
chchoudh22 <30825608+chchoudh22@users.noreply.github.com>
2018-01-21 11:20:03 +0530

    updated  login component

--- vPrj/app.js
+++ vPrj/app.js
@@ -3,96 +3,20 @@ var firstComponent = Vue.component('login-reg', {
     'template': `<div>
                     <div><loader-comp :state = "loaderState" size = "big"></loader-comp></div>
 				     <div>
-						<div class="col-md-6 col-xs-12 col-lg-offset-3 col-lg-6 col-md-offset-3">
-							<div class="panel panel-login">
-								<div class="panel-heading">
-									<div class="row">
-										<div class="col-xs-6">
-										<a href="#/login" :class= activeClassLogin id="login-form-link" v-on:click="toggleView('login')">Login</a>
-										</div>
-										<div class="col-xs-6">
-										<a href="#/login" id="register-form-link" :class= activeClassRegister v-on:click="toggleView('register')">Register</a>
-										</div>
-									</div>
-									<hr>
-									
-								</div> 
-								<div>
-								<div v-if ="message"><alert-comp :visibility = message :state = "state"></alert-comp></div>
-								</div>
-								<div class="panel-body login">
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-lg-offset-4 col-md-offset-4 col-sm-offset-4">
+						<div class="panel-body login">
                         <div class="row">
                             <div class="col-lg-12">
-                                <form id="login-form" role="form" class="" v-if='view=="login"'>
-                                   <div class="input-group my-input form-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input id="login-username" type="text" class="form-control body1" name="username" value="" placeholder="username or email" required>
-                                </div>
-
-                                <div class="input-group my-input form-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                    <input id="login-password" type="password" class="form-control body1" name="password" placeholder="password" required>
-                                </div>
-                                    <div class="form-group text-center">
-                                        <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                                        <label for="remember" class="body2"> Remember Me</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-sm-4 col-sm-offset-4 col-lg-6 col-lg-offset-3 col-xs-offset-3 col-xs-6">
-                                                <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login primary-btn disabled interactive-text" value="Log In" >
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-lg-12">
-                                                <div class="text-center">
-                                                    <a href="#" tabindex="5" class="forgot-password body3">Forgot Password?</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-				                    <div class="row">
-                                        <div class="col-sm-6 col-xs-12"><img src="asset/google.png" class="img-responsive" v-on:click="gSignin()" id="google-img"/></div>
-                                        <div class="col-sm-6 col-xs-12 disabledOpacity"><img src="asset/Login_WithFacebook.png" class="img-responsive" id="fb-img"/></div>
-                                    </div>
+                                <form id="google-login-form" role="form">
+                                 <div class="row"><div class="col-md-12"><center><h3 class='loginHeader'>Login to Your Account.</h3></center></div>
+                                 <div class="col-md-12 col-sm-12 col-xs-12" ><center><img class="profile-img" src="asset/defaultAvatar.png" alt=""></center></div>
+                                 <div class="col-sm-12 col-xs-12"><img src="asset/google.png" id="google-img" class="img-responsive" v-on:click="gSignin()"></div> </div>
                                 </form>
-                                <form id="register-form" role="form" v-if='view=="register"'>
-                                    <!-- <div class="form-group">
-                                        <input type="text" name="username" id="username" tabindex="1" class="form-control body1" placeholder="Username" value="">
-                                    </div> -->
-                                     <div class="form-group">
-                                        <input type="text" name="firstname" id="firstname" class="form-control body1" placeholder="Firstname" value="" required >
-                                    </div>
-                                     <div class="form-group">
-                                        <input type="text" name="lastname" id="lastname" tabindex="1" class="form-control body1" placeholder="Lastname" value="" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="email" id="email" tabindex="1" class="form-control body1" placeholder="Email Address" value="" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" name="dob" id="dob" tabindex="1" class="form-control date-picker body1" placeholder="Birth Date" value="" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="password" id="password" tabindex="2" class="form-control body1" placeholder="Password" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control body1" placeholder="Confirm Password" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-sm-6 col-sm-offset-3">
-                                                <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn primary-btn interactive-text" value="Register Now">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
+                                </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 							</div>
-						</div>				
+						</div>		
 					</div>
 				</div>
 				`,
